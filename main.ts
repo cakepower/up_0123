c11251cb2b0eea3cb60cf04359f3927ce9902c63
cakepower <32028127+cakepower@users.noreply.github.com>
2022-11-24 16:00:23 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 5) {
+        basic.showString("CLAP")
+    } else {
+        basic.showString("")
+    }
+})
+radio.setGroup(78)
 let strip = neopixel.create(DigitalPin.P13, 24, NeoPixelMode.RGB)
 let light_s = 0
 strip.setBrightness(40)
@@ -8,6 +16,7 @@ basic.forever(function () {
     if (light_s > 999) {
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         strip.show()
+        radio.sendNumber(5)
     } else {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
         strip.show()
